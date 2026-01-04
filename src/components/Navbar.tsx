cb91c73c0b59225f9e8cae0c0f6d3fbbfea82a2b
Harsh Nawle <harshnawle09@gmail.com>
2026-01-05 00:04:08 +0530

    feat: Navbar work done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Avatar, AvatarImage } from './ui/avatar'
+import ProfileImage from '../assets/(2) Instagram.jpeg'
+
+import { ModeToggle } from './ToggleMode'
+
+
+const Navbar = () => {
+
+
+
+    return (
+        <div className='bg-transparent flex gap-7 items-center justify-between container mx-auto max-w-3xl p-7'>
+            <div className='flex items-baseline gap-6'>
+                <div>
+                    <a href="#">
+                        <img src={ProfileImage}
+                            className="size-[60px] rounded-xl transition-transform duration-300 hover:scale-90" />
+                    </a>
+                </div>
+                <div>
+                    <div className=' flex gap-4 items-baseline justify-center font-[400px]'>
+                        <a href="#about" className='hover:underline hover:underline-offset-4 text-black dark:text-gray-300 decoration-2' >About</a>
+                        <a href="#project" className='hover:underline hover:underline-offset-4 text-black dark:text-gray-300 decoration-2' >Project</a>
+                        <a href="#work" className='hover:underline hover:underline-offset-4 text-black dark:text-gray-300 decoration-2' >Work</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex items-center justify-center '>
+                <ModeToggle />
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
