0fbf9ea3548192cff33da7a8fcbb8ee64696af2b
Harsh Nawle <harshnawle09@gmail.com>
2026-01-07 23:20:30 +0530

    feat: add new project section with project cards and associated assets.

--- src/components/ProjectComponents/Project01.tsx
+++ src/components/ProjectComponents/Project01.tsx
@@ -1,82 +1,96 @@
-import { Github, Globe } from 'lucide-react'
-import project1 from '../../assets/project-1.webp'
-import nextjs from '../../assets/nextjs.svg'
-import typescript from '../../assets/typescript.svg'
+import { ArrowRightIcon, Github, Globe } from 'lucide-react'
+import Project1 from '../../assets/project-1.webp'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+// import nextjs from '../../assets/nextjs.svg'
+// import typescript from '../../assets/typescript.svg'
 import react from '../../assets/react.svg'
 import mongodb from '../../assets/mongodb.svg'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card'
-import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+import nodeJS from '../../assets/nodejs.svg'
 
 
 const Project01 = () => {
     return (
         <div>
-            <Card className='max-w-md pt-0'>
-                <CardContent className='px-0'>
-                    <img
-                        src={project1}
-                        alt='Banner'
-                        className='aspect-video h-60 rounded-t-xl object-cover'
-                    />
-                </CardContent>
-                <CardHeader>
-                    <CardTitle>
-                        <div className='flex justify-between'>
-                            <div>
-                                <h3 className='text-xl font-semibold leading-tight'>
-                                    Huddle - chat me
-                                </h3>
-                            </div>
-                            <div className='flex gap-2 items-center'>
-                                <a href="" className='text-gray-400'>
+            <div className="bg-card text-card-foreground flex flex-col gap-6
+            rounded-xl border group h-full w-full overflow-hidden transition-all
+            p-0 border-gray-100 dark:border-gray-800 shadow-none">
+                <div className="grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6 p-0">
+                    <div className="group relative aspect-video overflow-hidden">
+                        <img src={Project1} className="w-full h-full object-cover" />
+                    </div>
+                </div>
+                <div className='px-6'>
+                    <div className='space-y-4'>
+                        <div className='flex items-center justify-between gap-4'>
+                            <h3 className='text-2xl font-semibold leading-tight group-hover:text-primary hover:cursor-pointer'>
+                                Huddle
+                            </h3>
+                            <div className='flex items-center gap-2'>
+                                <div>
                                     <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <Globe className='size-6' />
-
+                                        <TooltipTrigger>
+                                            <button>
+                                                <a href="" className='text-gray-400 flex size-6 items-center justify-center hover:text-primary transition-colors duration-200 ease-[ease]'>
+                                                    <Globe />
+                                                </a>
+                                            </button>
                                         </TooltipTrigger>
                                         <TooltipContent>
                                             <p>View Website</p>
                                         </TooltipContent>
                                     </Tooltip>
-                                </a>
-                                <a href="" className='text-gray-400'>
+                                </div>
+
+                                <div>
                                     <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <Github className='size-6' />
-
-
+                                        <TooltipTrigger>
+                                            <button>
+                                                <a href="" className='text-gray-400 flex size-6 items-center justify-center hover:text-primary transition-colors duration-200 ease-[ease]'>
+                                                    <Github />
+                                                </a>
+                                            </button>
                                         </TooltipTrigger>
                                         <TooltipContent>
                                             <p>View Github</p>
                                         </TooltipContent>
                                     </Tooltip>
-                                </a>
+                                </div>
                             </div>
                         </div>
-                    </CardTitle>
-                    <CardDescription>Smooth, flowing gradients blending rich reds and blues in an abstract swirl.</CardDescription>
-                </CardHeader>
-                <CardFooter className='flex-col items-start gap-4'>
-                    {/* Technologies */}
-                    <div className="w-full">
-                        <p className="text-xs font-semibold text-gray-500 tracking-wider mb-2">Technologies</p>
-                        <div className="flex gap-1 items-center">
-                            <div className="size-10   flex items-center justify-center p-1.5" title="React">
-                                <img src={react} alt="React" className="w-full h-full object-contain" />
-                            </div>
-                            <div className="size-10 flex items-center justify-center p-1.5 " title="TypeScript">
-                                <img src={typescript} alt="TS" className="w-full h-full object-contain" />
-                            </div>
-                            <div className="size-10 flex items-center justify-center p-1.5 " title="Next.js">
-                                <img src={nextjs} alt="Next" className="w-full h-full object-contain filter invert dark:invert-0" />
-                            </div>
-                            <div className="size-10 flex items-center justify-center p-1.5 " title="MongoDB">
-                                <img src={mongodb} alt="Mongo" className="w-full h-full object-contain" />
+                        <p className='text-gray-400 line-clamp-3'>
+                            A comprehensive study platform with notes, flashcards, quizzes, AI chatbot, and interactive learning tools
+                        </p>
+                        <div>
+                            <h4 className='text-sm font-medium mb-2 text-gray-400'>
+                                Technologies
+                            </h4>
+                            <div className='flex flex-wrap gap-2'>
+                                <div className='size-6 hover:scale-120 hover:cursor-pointer transition-transform duration-200 ease-[ease]'>
+                                    <img src={react} alt="" />
+                                </div>
+                                <div className='size-6 hover:scale-120 hover:cursor-pointer transition-transform duration-200 ease-[ease]'>
+                                    <img src={mongodb} alt="" />
+                                </div>
+                                <div className='size-6 hover:scale-120 hover:cursor-pointer transition-transform duration-200 ease-[ease]'>
+                                    <img src={nodeJS} alt="" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </CardFooter>
-            </Card>
+                </div>
+
+                <div className='items-center [.border-t]:pt-6 p-6 pt-0 flex justify-between'>
+                    <div className='flex items-center gap-1 rounded-md px-2 py-1 text-xs border-green-300 bg-green-500/10'>
+                        <div className='size-2 rounded-full bg-green-500 animate-pulse'></div>
+                        All Systems Operational
+                    </div>
+                    <a href=" " className='text-gray-400 flex items-center gap-2 text-sm hover:underline underline-offset-4 hover:text-primary transition-colors duration-200 ease-[ease]'>
+                        View Details
+                        <ArrowRightIcon/>
+                    </a>
+                </div>
+            </div>
+
 
         </div>
     )
--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,11 +1,10 @@
 import Project01 from "./ProjectComponents/Project01"
-import Project02 from "./ProjectComponents/Project02"
 
 
 const ProjectSection = () => {
     return (
         // min-h-screen remember to add in  below div
-        <div className="container mx-auto max-w-4xl px-12 max-auto ">
+        <div className="container mx-auto max-w-4xl px-12 max-auto min-h-screen ">
             <div>
                 <p className="text-gray-400 text-sm">Featured</p>
                 <h2 className="text-2xl font-bold">Projects</h2>
@@ -15,7 +14,8 @@ const ProjectSection = () => {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 mt-8">
                     {/* Card section */}
                     <Project01/>
-                    <Project02/>
+                    <Project01/>
+                    <Project01/>
                 </div>
 
             </div>
